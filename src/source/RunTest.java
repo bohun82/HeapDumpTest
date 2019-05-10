322e77e29625718c43c9eda83933a369c8334cb0
PiotrWojcik <piotr.wojcik.2@asseco.pl>
2019-05-10 08:01:56 +0200

    Simple application to create heap dump

---
+++ src/source/RunTest.java
@@ -0,0 +1,20 @@
+package source;
+
+import java.util.ArrayList;
+
+public class RunTest {
+
+    public static void main(String[] args){
+        RunTest runTest = new RunTest();
+        runTest.simpleTest();
+    }
+
+    private void simpleTest() {
+        ArrayList arrayList = new ArrayList();
+        for (int i=0; i < 10000000; i++){
+            arrayList.add(i);
+        }
+    }
+
+
+}
